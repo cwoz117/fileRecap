cc8ed5c9b52ac20ba7e7c54fb4cf0337d0da5720
Chris Wozniak <cwoz117@hotmail.ca>
2015-09-15 13:43:24 -0600

    basic file io example

--- FileIOExample.java
+++ FileIOExample.java
@@ -1,1 +1,25 @@
-import java.io.*;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.io.IOException;
+
+class FileIOExample{
+
+    public static  void main(String[] args)throws IOException{
+	FileReader inFileReader = new FileReader("infile.dat");
+	Scanner inFile = new Scanner(inFileReader);
+
+
+
+	PrintWriter outFile = new PrintWriter(new FileWriter("outfile.dat"));
+
+	while (inFile.hasNextLine()){
+	    outFile.println(inFile.nextLine());
+
+	}
+	inFile.close();
+	outFile.close();
+    }
+
+}
